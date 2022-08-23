72519e2eabe667779e1f7d4e087c5e4fa8344ff0
bdfd <bdfd2005@gmail.com>
2022-08-23 16:28:21 -0400

    Init Commit

--- nav.config.ts
+++ nav.config.ts
@@ -1,9 +1,19 @@
+/*
+ * @Date         : 2022-08-23 16:26:51
+ * @Author       : BDFD,bdfd2005@gmail.com
+ * @Github       : https://github.com/bdfd
+ * @LastEditTime : 2022-08-23 16:28:01
+ * @LastEditors  : BDFD
+ * @Description  : 
+ * @FilePath     : \nav.config.ts
+ * Copyright (c) 2022 by BDFD, All Rights Reserved. 
+ */
 import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/bdfd/FrondEnd-Navigation-Page',
+  gitRepoUrl: 'https://github.com/bdfd/NavPage',
 
   // Deployment branch name
   // 部署分支
